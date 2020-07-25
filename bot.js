a5e1257b0f90c3e851d18698371df9b06e4c670d
roofingoof <subhanafzal125@gmail.com>
2020-07-25 17:34:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,39 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+
+function randomize(){
+    var number = Math.floor(Math.random() * 11);
+    if(number >5){
+        return true;
+    }
+}
+
 bot.once("ready", () => {
     console.log("started");
 });
 
-
+ 
 bot.on("message", message => {
-    if(message.author.id == "268402596312645633 "){
+    if(message.author.id == "268402596312645633"){
+        if(randomize()){
+            message.delete();
+        }
         message.react("736209309498736681");
+    }
+});
+
+
+bot.on("voiceStateUpdate", (oldMember,newMember) =>{
+    let newUserChannel = newMember.channel
+    let oldUserChannel = oldMember.channel
+    if(oldUserChannel === undefined && newUserChannel !== undefined) {
+        // user joins channel
+        if(newMember.member.id == "268402596312645633"){
+            if(randomize()){
+                newMember.kick();
+            }
+        }
     }
 });
 
