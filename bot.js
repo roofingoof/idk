77759b289056a2f4e776b718f7a9dcca4da10da3
roofingoof <subhanafzal125@gmail.com>
2020-07-26 19:05:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const bot = new Discord.Client();
 
 const target = "268402596312645633"
 
-function randomize(){
-    var number = Math.floor(Math.random() * 11);
-    if(number >5){
+function randomize(percentage){
+    var number = Math.floor(Math.random() * 101);
+    if(percentage >= number){
         return true;
     }
 }
@@ -17,10 +17,12 @@ bot.once("ready", () => {
  
 bot.on("message", message => {
     if(message.author.id == target){
-        if(randomize()){
+        if(randomize(20)){
             message.delete();
+            return;
         }
         message.react("736209309498736681");
+        return;
     }
 });
 
@@ -32,13 +34,29 @@ bot.on("voiceStateUpdate", (oldMember,newMember) =>{
 
     if(oldUserChannel == undefined && newUserChannel !== undefined){
         if(newMember.member.id == target){
-            if(randomize()){
+            if(randomize(10)){
                 newMember.kick();
+                return;
+            }else if(randomize(15)){
+                newMember.setMute(true);
+                setTimeout(function(){
+                    newMember.setMute(false);
+                },5000);
+            }else if(randomize(18)){
+                newMember.setDeaf(true);
+                setTimeout(function(){
+                    newMember.setDeaf(false);
+                },5000);
+            }else if(randomize(20)){
+                var vc = newMember.guild.channels.cache.find(channel => channel.name === "Retarded Normies");
+                newMember.setChannel(vc);
             }
+            
+            
+
         }
     }
 });
-
 
 
 bot.login(process.env.token);
